2d181efb83541d0a0729fe10b9ad388a23e7bbb1
Thomas Moore <tomo@pagu.co>
2016-12-17 15:49:45 +0000

    Just use the same link color for both github & project

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="project pa4" v-bind:style="{ background: bg, color: color, textAlign: textalign }">
-    <h3 class="f2 mt0">{{name}}<span v-if="github">&nbsp;<a v-bind:href="github" v-bind:title="'Check out ' + name + ' on Github'"><i class="fa fa-github" aria-hidden="true"></i></a></span></h3>
+    <h3 class="f2 mt0">{{name}}<span v-if="github">&nbsp;<a v-bind:style="{ color: color }" v-bind:href="github" v-bind:title="'Check out ' + name + ' on Github'"><i class="fa fa-github" aria-hidden="true"></i></a></span></h3>
     <p class="lead">{{tag}}</p>
     <a v-if="link" v-bind:href="link" v-bind:title="'View ' + name" v-bind:style="{ color: color }" class="mb0">View Project</a>
   </div>
@@ -23,8 +23,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  :link:hover,
-  :visited:hover {
-    color: #FFD034;
-  }
 </style>
