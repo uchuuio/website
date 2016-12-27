934ad642cb3bafa4a9c4b691367702e12ee93776
Thomas Moore <tomo@pagu.co>
2016-12-27 21:17:01 +0000

    Update packages + karma-coveralls

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
     // 2. add it to the `browsers` array below.
     browsers: ['PhantomJS'],
     frameworks: ['mocha', 'sinon-chai'],
-    reporters: ['spec', 'coverage'],
+    reporters: ['spec', 'coverage', 'coveralls'],
     files: ['./index.js'],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
